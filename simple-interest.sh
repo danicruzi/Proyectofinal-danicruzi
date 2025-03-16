9a1fbe780f6a3e3c2389775b642275a4b500cda9
danicruzi <dani.cruz0207@gmail.com>
2025-03-15 18:46:17 -0600

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+# Script para calcular interés simple
+
 echo "Introduce el capital inicial:"
 read principal
+
 echo "Introduce la tasa de interés (en porcentaje):"
 read rate
+
 echo "Introduce el tiempo (en años):"
 read time
-interest=$(echo "$principal * $rate * $time / 100" | bc)
+
+# Cálculo del interés simple
+interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+
 echo "El interés simple es: $interest"
+
